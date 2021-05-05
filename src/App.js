4fac3d3102fc0dad7dec34bb98ba433adccc1acd
Paulo Rodrigues <paulo.rodrigues@rebase.com.br>
2021-05-04 21:41:03 -0300

    context example

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
 import './App.css';
-import React, { useState} from 'react'
+import React, { useState, createContext, useContext } from 'react'
 // import Button from './components/Button'
-import Counter from './components/Counter'
+// import Counter from './components/Counter'
 // import AddressForm from './components/AddressForm'
 
+// function App() {
+//   const [visible, setVisible] = useState(false)
+//   return (
+//     <>
+//       <button onClick={() => setVisible(!visible)}>Show/Hide</button>
+//       <p>Counter</p>
+//       { visible && <Counter />}
+//     </>
+//   )
+// }
+
+const NameContext = createContext()
+
 function App() {
-  const [visible, setVisible] = useState(false)
+  const [name, setName] = useState('Billy')
   return (
-    <>
-      <button onClick={() => setVisible(!visible)}>Show/Hide</button>
-      <p>Counter</p>
-      { visible && <Counter />}
-    </>
+    <NameContext.Provider value={name}>
+      <Child />
+    </NameContext.Provider>
+  )
+}
+
+function Child() {
+  return (
+    <section>
+      <Grandchild />
+    </section>
+  )
+}
+
+function Grandchild() {
+  return (
+    <div>
+      <Button />
+    </div>
+  )
+}
+
+function Button() {
+  const name = useContext(NameContext)
+  return (
+    <button>{name}</button>
   )
 }
 
